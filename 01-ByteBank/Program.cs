76a49f43db97917e1fbbf705a72c8923b1c6fffd
pnomellini <paulo@nomellini.net>
2022-03-19 17:59:30 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente(); 
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+            
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agencia: "+contaDaGabriela.agencia);
+            Console.WriteLine("Numero: "+contaDaGabriela.numero);
+            Console.WriteLine("Saldo: "+contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 100;
+
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);   
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 863452;
+
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            int idade = 27;
+            int idadeDiferente = 27; 
+
+            Console.WriteLine(idade == idadeDiferente);
+
+            Console.ReadLine();
+        }
+    }
+}
